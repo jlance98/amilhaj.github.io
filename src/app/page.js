f34d996a232b760621e22a0c4076ce7b2573e52b
Joshua Amilhasan <amilhasan.joshua@gmail.com>
2023-08-31 20:38:48 -0400

    Changed intro page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,62 @@
-import Styles from "./page.module.css";
-import AboutCard from "../components/about-card";
-import Projects from "../components/projects";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="container">
-			<AboutCard />
-			<h1>Projects</h1>
-			<Projects />
+		<div className="content">
+			<div id="intro">
+				<div>
+					<h1 style={{ marginBottom: 0 }}> JOSHUA</h1>
+					<h1>AMILHASAN</h1>
+					<h2 style={{ borderBottom: "solid", borderColor: "black" }}>
+						Full-Stack Web Developer
+					</h2>
+
+					<ul>
+						<li>
+							<a href="https://github.com/amilhaj">
+								<div className="image-wrapper">
+									<Image
+										src="/github-mark.png"
+										fill={true}
+										alt="Github Logo"
+									/>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="https://www.linkedin.com/in/joshua-amilhasan/">
+								<div className="image-wrapper">
+									<Image
+										src="/linkedin-mark.png"
+										fill={true}
+										alt="LinkedIn Logo"
+									/>
+								</div>
+							</a>
+						</li>
+						<li>
+							<a href="mailto: amilhasan.joshua@gmail.com">
+								<div className="image-wrapper">
+									<Image
+										src="/email-mark.png"
+										fill={true}
+										alt="E-Mail Logo"
+									/>
+								</div>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div id="skills">
+				<h1>Skills</h1>
+				<h2>Full-Stack Web Developer</h2>
+			</div>
+			<div id="">
+				<h1>Ends here</h1>
+			</div>
 		</div>
 	);
 }
--- src/components/navbar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./component.css";
-
-const navbar = () => {
-	return <></>;
-};
-
-export default navbar;
