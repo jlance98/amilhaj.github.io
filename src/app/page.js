039697c3252330a37061093699493a8a49e3f045
Joshua Amilhasan <amilhasan.joshua@gmail.com>
2023-09-03 23:23:20 -0400

    Added intro and skills page

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,17 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="content">
-			<div id="intro">
+			<div className="intro">
 				<div>
 					<h1 style={{ marginBottom: 0 }}> JOSHUA</h1>
 					<h1>AMILHASAN</h1>
-					<h2 style={{ borderBottom: "solid", borderColor: "black" }}>
-						Full-Stack Web Developer
+					<h2
+						style={{
+							borderBottom: "solid",
+							borderColor: "rgb(51,51,51)",
+						}}
+					>
+						Full-Stack Developer
 					</h2>
 
 					<ul>
@@ -50,12 +55,103 @@ export default function Home() {
 					</ul>
 				</div>
 			</div>
-			<div id="skills">
-				<h1>Skills</h1>
-				<h2>Full-Stack Web Developer</h2>
+			<div className="skills">
+				<div>
+					<h1>Skills</h1>
+					<ul className="skill-categories">
+						<div>
+							<li className="category">
+								<h2
+									style={{
+										borderLeft: "solid",
+										borderColor: "rgb(51,51,51)",
+										paddingLeft: "1rem",
+									}}
+								>
+									Front-End
+								</h2>
+								<ul>
+									<li>HTML5</li>
+									<li>CSS3 + Bootstrap</li>
+									<li>JavaScript</li>
+									<li>React.js</li>
+									<li>Next.js</li>
+								</ul>
+							</li>
+						</div>
+						<div>
+							<li className="category">
+								<h2
+									style={{
+										borderLeft: "solid",
+										borderColor: "rgb(51,51,51)",
+										paddingLeft: "1rem",
+									}}
+								>
+									Back-End
+								</h2>
+								<ul>
+									<li>C# + ASP.NET</li>
+									<li>Java</li>
+									<li>Python</li>
+									<li>SQL</li>
+									<li>MongoDB</li>
+									<li>RESTful APIs</li>
+								</ul>
+							</li>
+						</div>
+						<div>
+							<li className="category">
+								<h2
+									style={{
+										borderLeft: "solid",
+										borderColor: "rgb(51,51,51)",
+										paddingLeft: "1rem",
+									}}
+								>
+									Tools
+								</h2>
+								<ul>
+									<li>Git</li>
+									<li>Docker</li>
+									<li>GitHub Actions</li>
+									<li>Maven</li>
+									<li>Visual Studio & VS Code</li>
+									<li>Jira</li>
+								</ul>
+							</li>
+						</div>
+					</ul>
+				</div>
 			</div>
-			<div id="">
-				<h1>Ends here</h1>
+			<div className="footer">
+				<footer class="py-3 my-4">
+					<ul class="nav justify-content-center pb-3 mb-3">
+						<li class="nav-item">
+							<a href="#intro" class="nav-link px-2 text-muted">
+								Home
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link px-2 text-muted">
+								About
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link px-2 text-muted">
+								Skills
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link px-2 text-muted">
+								Projects
+							</a>
+						</li>
+					</ul>
+					<p class="text-center text-muted">
+						&copy; 2023 Joshua Amilhasan
+					</p>
+				</footer>
 			</div>
 		</div>
 	);
